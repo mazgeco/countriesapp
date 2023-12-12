df8c76cc56b63d77615e800b6600044267097414
Gabby Zapata <gabby.ecuador@gmail.com>
2023-12-12 17:23:00 -0500

    continuación de capacitación Angular

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, count, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -9,6 +9,15 @@ export class CountriesService {
   private apiURL: string  = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
+
+  searchCountryByAlphaCode(code: string): Observable<Country | null>{
+    const url = `${this.apiURL}/alpha/${code}`;
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(error => of(null))
+      );
+  }
 
   searchCountry(type: string, term: string): Observable<Country[]>{
     const url = `${this.apiURL}/${type}/${term}`;
